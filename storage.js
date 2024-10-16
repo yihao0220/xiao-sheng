bfb5c253504e7b48f59c3c562978b3e1d52ee1ad
yihao0220 <wy368375@gmail.com>
2024-10-17 00:16:12 +0800

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,5 +1,12 @@
 const Storage = {
-    setItem: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-    getItem: (key) => JSON.parse(localStorage.getItem(key)),
+    setItem: (key, value) => {
+        console.log(`Setting ${key} in storage:`, value);
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    getItem: (key) => {
+        const value = JSON.parse(localStorage.getItem(key));
+        console.log(`Getting ${key} from storage:`, value);
+        return value;
+    },
     removeItem: (key) => localStorage.removeItem(key)
 };
